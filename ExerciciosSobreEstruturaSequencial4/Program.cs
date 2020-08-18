6dfa0ae4f22e48443830509b505e60f356aa0376
Andreia de Moraes <andreiadmrs@gmail.com>
2020-08-18 00:05:03 -0300

    exercicio do curso da udemy csharp

---
+++ ExerciciosSobreEstruturaSequencial4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExerciciosSobreEstruturaSequencial4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* Fazer um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por
+ hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas
+decimais.*/
+
+            Console.WriteLine("Digite abaixo seu número:");
+           int numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite abaixo a quantidade de horas trabalhadas:");
+            int horas = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite abaixo o valor por hora:");
+            double valorPorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            double salario = horas * valorPorHora;
+
+
+            Console.WriteLine("Number "+numero+ "Salary is $" + salario.ToString("F2", CultureInfo.InvariantCulture)) ;
+
+        }
+    }
+}
